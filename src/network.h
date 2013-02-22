ae87890121222b57206d40dc75db0490926ed05a
jaekyu <jaekyu@76505255-db3a-46bb-8524-017587b0c4bf>
2013-02-22 16:54:23 +0000

    1. memory/noc/dram decoupled
    git-svn-id: https://svn.research.cc.gatech.edu/macsim/trunk@364 76505255-db3a-46bb-8524-017587b0c4bf

--- src/network.h
+++ src/network.h
@@ -28,16 +28,17 @@ POSSIBILITY OF SUCH DAMAGE.
 
 
 /**********************************************************************************************
- * File         : router.h 
- * Author       : Jaekyu Lee
- * Date         : 12/7/2011
+ * File         : network.h 
+ * Author       : HPArch Research Group
+ * Date         : 2/18/2013
  * SVN          : $Id: dram.h 867 2009-11-05 02:28:12Z kacear $:
  * Description  : Interconnection network
  *********************************************************************************************/
 
 
-#ifndef ROUTER_H
-#define ROUTER_H
+#ifndef NETWORK_H
+#define NETWORK_H
+
 
 #include <vector>
 #include <unordered_map>
@@ -51,6 +52,34 @@ POSSIBILITY OF SUCH DAMAGE.
 #define GPU_ROUTER 1
 #define L3_ROUTER 2
 #define MC_ROUTER 3
+
+#define LOCAL 0
+#define LEFT  1
+#define RIGHT 2
+#define UP    3
+#define DOWN  4
+
+#define HEAD 0
+#define BODY 1
+#define TAIL 2
+
+#define INIT 0
+#define IB   1
+#define RC   2
+#define VCA  3
+#define SA   4
+#define ST   5
+#define LT   6
+
+#define OLDEST_FIRST 0
+#define ROUND_ROBIN  1
+#define CPU_FIRST    2
+#define GPU_FIRST    3
+
+#define GPU_FRIENDLY 0
+#define CPU_FRIENDLY 1
+#define MIXED        2
+#define INTERLEAVED  3
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
@@ -114,175 +143,106 @@ class credit_c
 };
 
 
-bool router_sort_fn(router_c *a, router_c *b);
-
 ///////////////////////////////////////////////////////////////////////////////////////////////
-/// \brief Router class
+/// \brief dummy router class
 ///////////////////////////////////////////////////////////////////////////////////////////////
 class router_c
 {
-  friend class router_wrapper_c;
-
   public:
-    /**
-     * Constructor
-     */
-    router_c(macsim_c* simBase, int type, int id);
-
-    /**
-     * Destructor
-     */
+    router_c(macsim_c* simBase, int type, int id, int num_port);
     ~router_c();
 
-    /**
-     * Run a cycle (tick) function
-     */
-    void run_a_cycle();
-
-    /**
-     * Inject a packet into the network
-     */
-    bool inject_packet(mem_req_s* req);
-
-    /**
-     * Insert a packet into the next router
-     */
-    void insert_packet(flit_c* flit, int ip, int ivc);
-
-    /**
-     * Get router id
-     */
-    int get_id(void);
-
-    /**
-     * Set router id
-     */
-    void set_id(int id);
-
-    /**
-     * Eject a packet from the router
-     */
-    mem_req_s* receive_req(int dir = -1);
-
-    /**
-     * Pop a request
-     */
-    void pop_req(int dir = -1);
-
-    /**
-     * Initialize a router
-     */
-    void init(int total_router, int* total_packet, pool_c<flit_c>* flit_pool, pool_c<credit_c>* credit_pool);
-
-    /**
-     * Set links for a router
-     */
-    void set_link(int dir, router_c* link);
-
-    /**
-     * Printer the router information
-     */
-    void print(ofstream& out);
-
-    /**
-     * Print link information
-     */
-    void print_link_info();
-
-    void set_dstream_routers(vector<router_c *>&, int);
-
-    void set_ustream_routers(vector<router_c *>&, int);
-
-  private:
-    /**
-     * RC (Route Calculation) stage
-     */
-    void stage_rc(void);
-
-    /**
-     * Mesh Routing Calculation
-     */
-    void rc_mesh(void);
-
-    /**
-     * Ring Routing Calculation
-     */
-    void rc_ring(void);
+  public:
+    virtual bool inject_packet(mem_req_s* req);
+    virtual mem_req_s* receive_req(int dir);
+    virtual void pop_req(int dir);
+    virtual void init(int total_router, int* total_packet, pool_c<flit_c>* flit_pool, 
+        pool_c<credit_c>* credit_pool);
+    virtual void set_link(int dir, router_c* link);
+    virtual int get_id(void);
+    virtual void set_id(int id);
+    virtual void run_a_cycle(void);
+    virtual void print_link_info() = 0;
+    virtual void print(ofstream& out);
+    virtual void insert_packet(flit_c* flit, int ip, int ivc);
+    virtual void insert_credit(credit_c*);
+    virtual router_c* get_router(int dir);
     
     /**
      * VCA (Virtual Channel Allocation) stage
      */
-    void stage_vca(void);
-
+    virtual void stage_vca(void);
+    
     /**
-     * VC arbitration
+     * RC (Route Calculation) stage
      */
-    void stage_vca_pick_winner(int, int, int&, int&);
-
-    /**
-     * SWA (Switch Allocation) stage
-     */
-    void stage_sa(void);
-
-    /**
-     * SW arbitration
-     */
-    void stage_sa_pick_winner(int, int&, int&, int);
-
-    /**
-     * SWT (Switch Traversal) stage
-     */
-    void stage_st(void);
+    virtual void stage_rc(void) = 0;
 
     /**
      * LT (Link Traversal) stage
      */
-    void stage_lt(void);
+    virtual void stage_lt(void);
+    
+    /**
+     * SWT (Switch Traversal) stage
+     */
+    virtual void stage_st(void);
+    
+    /**
+     * VC arbitration
+     */
+    virtual void stage_vca_pick_winner(int, int, int&, int&) = 0;
 
     /**
-     * Get output virtual channel occupancy per type
+     * SW arbitration
      */
-    int get_ovc_occupancy(int port, bool type);
-
-    /**
-     * Check starvation
-     */
-    void check_starvation(void);
-
-    /**
-     * Check channel (for statistics)
-     */
-    void check_channel(void);
-
-    /**
-     * Process pending credits to model credit traversal latency
-     */
-    void process_pending_credit(void);
+    virtual void stage_sa_pick_winner(int, int&, int&, int);
 
     /**
      * Local packet injection
      */
-    void local_packet_injection(void);
-
-    void packet_injection(void);
-
-    bool try_packet_insert(int src, mem_req_s *req);
+    virtual void local_packet_injection(void);
 
     /**
-     * Insert a credit from previous router
+     * Process pending credits to model credit traversal latency
      */
-    void insert_credit(credit_c*);
+    virtual void process_pending_credit(void);
+
+    /**
+     * SWA (Switch Allocation) stage
+     */
+    virtual void stage_sa(void);
+
+    /**
+     * Get output virtual channel occupancy per type
+     */
+    virtual int get_ovc_occupancy(int port, bool type);
+
+    /**
+     * Check channel (for statistics)
+     */
+    virtual void check_channel(void);
+
+    /**
+     * Check starvation
+     */
+    virtual void check_starvation(void);
 
   private:
+    router_c(); // do not implement
+
+  protected:
     macsim_c* m_simBase; /**< pointer to simulation base class */
+
+    int m_num_vc; /**< number of virtual channels */
+    int m_num_port; /**< number of ports */
+    
     string m_topology; /**< router topology */
     int m_type; /**< router type */
     int m_id; /**< router id */
     int m_total_router; /**< number of total routers */
 
     // configurations
-    int m_num_vc; /**< number of virtual channels */
-    int m_num_port; /**< number of ports */
     int m_link_latency; /**< link latency */
     int m_link_width; /**< link width */
     int* m_total_packet; /**< number of total packets (global) */
@@ -330,100 +290,63 @@ class router_c
     // clock
     Counter m_cycle; /**< router clock */
 
-    // additions for validation
-    vector<router_c *> m_dstream; /**< down stream routers */
-    int m_ds_start;
-    vector<router_c *> m_ustream; /**< up stream routers */
-    int m_us_start;
-    queue<mem_req_s *> m_us_injection_buffer;
-    queue<mem_req_s *> m_ds_injection_buffer;
-    int m_ds_injection_buffer_max_size;
-    int m_us_injection_buffer_max_size;
-
-    Counter m_recv_from_us_avail;
-    Counter m_recv_from_ds_avail;
-    Counter m_send_to_us_avail;
-    Counter m_send_to_ds_avail;
-
-    queue<mem_req_s *> m_packets_from_us;
-    queue<mem_req_s *> m_packets_from_ds;
-    int m_max_pending_flits_from_us;
-    int m_max_pending_flits_from_ds;
-    int m_pending_flits_from_us;
-    int m_pending_flits_from_ds;
 };
 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
-/// \brief Router Wrapper Class
+/// \brief Network interface
 ///////////////////////////////////////////////////////////////////////////////////////////////
-class router_wrapper_c
+class network_c
 {
   public:
-    /**
-     * Constructor
-     */
-    router_wrapper_c(macsim_c* simBase);
+    network_c(macsim_c* simBase)
+      : m_simBase(simBase)
+    {
+    }
 
-    /**
-     * Destructor
-     */
-    ~router_wrapper_c();
-
-    /**
-     * Run a cycle
-     */
-    void run_a_cycle(void);
-
-    /**
-     * Create a router
-     */
-    router_c* create_router(int type);
-
-    /**
-     * Initialize interconnection network
-     */
-    void init(void);
-
-    /**
-     * Ring initialization
-     */
-    void init_ring(void);
-
-    /**
-     * Mesh initialization
-     */
-    void init_mesh(void);
-
-    /**
-     * simple topology for validation
-     */
-    void init_simple_topology(void);
-
-    /**
-     * Print all router information
-     */
-    void print(void);
-
-    void insert_into_router_req_buffer(int router_id, mem_req_s *req);
+    ~network_c()
+    {
+    }
 
   private:
-    router_wrapper_c(); // do not implement
+    network_c();
 
-  private:
-    macsim_c* m_simBase; /**< sim base */
+  public:
+    virtual void init(int num_cpu, int num_gpu, int num_l3, int num_mc) = 0;
+
+    virtual bool send(mem_req_s* req, int src_level, int src_id,
+        int dst_level, int dst_id);
+
+    virtual mem_req_s* receive(int level, int id);
+
+    virtual void receive_pop(int level, int id);
+
+    virtual void run_a_cycle() = 0;
+
+    virtual void print() = 0;
+
+  protected:
+    macsim_c* m_simBase;
     string m_topology; /**< topology */
-    vector<router_c*> m_router; /**< all routers */
+    
     int m_num_router; /**< number of routers */
     int m_total_packet; /**< number of total packets */
+    int m_num_cpu;
+    int m_num_gpu;
+    int m_num_l3;
+    int m_num_mc;
+
     pool_c<flit_c>* m_flit_pool; /**< flit data structure pool */
     pool_c<credit_c>* m_credit_pool; /**< credit pool */
+    
+    vector<router_c*> m_router; /**< all routers */
+    unordered_map<int, int> m_router_map;
 
     Counter m_cycle; /**< clock cycle */
 
-    vector<router_c*> m_cores;
-    vector<router_c*> m_caches;
-    vector<router_c*> m_mcs;
 };
+
+
+
 
 #endif
